e6e49baf8f7aab092364f2e940ccfb306effd0e2
Felix Reidl <felix.reidl@gmail.com>
2022-03-15 00:21:19 +0000

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+pub mod pygraph;
+pub mod pyordgraph;
+
+#[cfg(not(test))] // pyclass and pymethods break `cargo test`
+#[pymodule]
+fn platypus(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<pygraph::PyGraph>()?;
+    m.add_class::<pygraph::PyVertexMapDegree>()?;
+    m.add_class::<pygraph::PyVertexMapBool>()?;
+    m.add_class::<pyordgraph::PyOrdGraph>()?;
+    // m.add_wrapped(wrap_pyfunction!(from_pid))?;
+
+    Ok(())
+}
